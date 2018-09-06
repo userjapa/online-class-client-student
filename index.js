91bf43bd77eb23561cd73fdd976636716683acd5
usuarioJapa <willian.hsimabukuro@gmail.com>
2018-09-06 13:25:56 -0300

    fixed static sercer

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const app = require('express')()
-const serveStatic = require('serve-static')
-const path = require('path')
+const app = require('express')(),
+      serveStatic = require('serve-static'),
+      path = require('path')
 
 const PORT = process.env.PORT?process.env.PORT:'8081'
 
-app.use(serveStatic(path.join(__dirname, 'dist')))
+app.use(serveStatic(__dirname))
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'))
